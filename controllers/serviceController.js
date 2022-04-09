70f28cdaf76b888e90903733c2889ca35bda9060
Ofir <ofir.duchovne@gmail.com>
2022-04-09 17:30:12 +0300

    new keys

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -8,21 +8,8 @@ const uploadFile = require('../services/aws.service');
 const jsEncrypt = new JSEncrypt();
 
 
-const privateKey = `-----BEGIN RSA PRIVATE KEY-----
-MIICXQIBAAKBgQDlOJu6TyygqxfWT7eLtGDwajtNFOb9I5XRb6khyfD1Yt3YiCgQ
-WMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76xFxdU6jE0NQ+Z+zEdhUTooNR
-aY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4gwQco1KRMDSmXSMkDwIDAQAB
-AoGAfY9LpnuWK5Bs50UVep5c93SJdUi82u7yMx4iHFMc/Z2hfenfYEzu+57fI4fv
-xTQ//5DbzRR/XKb8ulNv6+CHyPF31xk7YOBfkGI8qjLoq06V+FyBfDSwL8KbLyeH
-m7KUZnLNQbk8yGLzB3iYKkRHlmUanQGaNMIJziWOkN+N9dECQQD0ONYRNZeuM8zd
-8XJTSdcIX4a3gy3GGCJxOzv16XHxD03GW6UNLmfPwenKu+cdrQeaqEixrCejXdAF
-z/7+BSMpAkEA8EaSOeP5Xr3ZrbiKzi6TGMwHMvC7HdJxaBJbVRfApFrE0/mPwmP5
-rN7QwjrMY+0+AbXcm8mRQyQ1+IGEembsdwJBAN6az8Rv7QnD/YBvi52POIlRSSIM
-V7SwWvSK4WSMnGb1ZBbhgdg57DXaspcwHsFV7hByQ5BvMtIduHcT14ECfcECQATe
-aTgjFnqE/lQ22Rk0eGaYO80cc643BXVGafNfd9fcvwBMnk0iGX0XRsOozVt5Azil
-psLBYuApa66NcVHJpCECQQDTjI2AQhFc1yRnCU/YgDnSpJVm1nASoRUnU8Jfm3Oz
-uku7JUXcVpt08DFSceCEX9unCuMcT72rAQlLpdZir876
------END RSA PRIVATE KEY-----`
+const privateKey = fs.readFileSync(process.env.PRIVATE_KEY_PATH, 'utf8');
+const publicKey = fs.readFileSync(process.env.PUBLIC_KEY_PATH, 'utf8');
 
 
 jsEncrypt.setPrivateKey(privateKey);
@@ -51,7 +38,7 @@ exports.serviceController = {
 
     publicKey(req, res) {
         res.json({
-            publicKey: process.env.PUBLIC_KEY || "publicKey"
+            publicKey
         });
     },
     uploadHandler(req, res) {
--- frontend/scripts/upload_files.js
+++ frontend/scripts/upload_files.js
@@ -7,12 +7,6 @@ $(document).ready(function(e) {
         success:  function(data){
             console.log("upload_files.js");
             let publicKey = data.publicKey;
-            publicKey = `-----BEGIN PUBLIC KEY-----
-            MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDlOJu6TyygqxfWT7eLtGDwajtN
-            FOb9I5XRb6khyfD1Yt3YiCgQWMNW649887VGJiGr/L5i2osbl8C9+WJTeucF+S76
-            xFxdU6jE0NQ+Z+zEdhUTooNRaY5nZiu5PgDB0ED/ZKBUSLKL7eibMxZtMlUDHjm4
-            gwQco1KRMDSmXSMkDwIDAQAB
-            -----END PUBLIC KEY-----`;
             $("input[name='publicKey']").val(publicKey);
             $("#filesInput").prop('disabled', false); 
         },
