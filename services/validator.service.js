c1e4c49100e6c4e77197e9b8ee19ca47bd9853e4
Ofir <ofir.duchovne@gmail.com>
2022-04-09 13:11:33 +0300

    changed interface

--- services/validator.service.js
+++ services/validator.service.js
@@ -4,21 +4,19 @@ const fs = require('fs');
 const testResultSchema = JSON.parse(fs.readFileSync('./testResultSchema.json', 'utf8'));
 const cwd = process.cwd();
 
-
-const fileValidator = function(fileName) {
+const fileValidator = function(fileName, file) {
     const fileExtension = fileName.split('.').pop();
     if(fileExtension !== 'json') {
         return {valid:false, errors: [{message: 'File extension must be json'}]};
     }
     try {
-        const filePath = cwd + '/test-files/' + fileName;
-        const testResultFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        const testResultFile = JSON.parse(file);
         const valid = validator.validate(testResultFile, testResultSchema);
         return {valid: valid.valid, errors: valid.errors.map(err => '`'+(err.property.split('.')[1] || err.property) +'` ' + err.message).join('\n') || 'No errors'};
     } catch(e) {
         console.log('file is not valid');
         console.log(e);
-        return {valid:false, errors: [{message: 'Could not open file'}]};
+        return {valid:false, errors: [{message: 'Could not parse file'}]};
     }
 }
 
